8aeb4608427acdb47adab3af12c4d833edea4ec5
tiao2 <65841827@qq.com>
2024-09-22 14:01:42 +0800

    view.js

--- view.js
+++ view.js
@@ -1,3 +1,11 @@
 function BackgroundSet(nm,ve){
  eval('document.body.style.background'+nm+'="'+ve+'"')
 }
+
+function showBlock(block){
+  if(block=="widget"){ shown("zw"); } else if(block=="code"){ shown("zc"); } else if(block=="output"){ shown("zw"); } else { console.error("No such a block named "+block); };
+}
+
+function hideBlock(){
+
+}
